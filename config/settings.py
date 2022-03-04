2c38cee9cf676bfa8d2c15ce90057d5ffe99817a
JinHo Lee <lsyleo0210@gmail.com>
2022-03-04 22:52:57 +0900

    client added

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'journalist',
-    'ckeditor'
+    'ckeditor',
+    'client',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('journalist.urls'))
+    path('', include('client.urls'))
 ]
