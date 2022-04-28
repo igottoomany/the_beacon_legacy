306d5d81bcb27af802b1ee870bf5ecb915eeade2
JinHo Lee <lsyleo0210@gmail.com>
2022-04-28 16:19:09 +0900

    solved a dumb warning

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('journalist/', admin.site.urls),
     path('', include('client.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
