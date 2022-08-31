46a50ea60d8425d95890dc406c184d6a698f9ec5
Jinho Lee <lsyleo0210@gmail.com>
2022-08-31 21:29:16 +0900

    Revert "db update"
    This reverts commit 3ad129bcd3813bf9c4fd93d4b0a7adc833833560.

--- journalist/models.py
+++ journalist/models.py
@@ -7,7 +7,6 @@ from ckeditor.fields import RichTextField
 class Journalist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     dream_name = models.CharField(max_length=10, null=True)
-    eng_name = models.CharField(max_length=20, null=True)
     bio = models.TextField(max_length=255, null=True, blank=True)
 
     def __str__(self):
@@ -30,6 +29,7 @@ class Post(models.Model):
     content = RichTextUploadingField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.ForeignKey(Category, null=True, related_name='category', on_delete=models.SET_NULL)
+
 
     def __str__(self):
         return str(self.title) + ' | ' + str(self.author)
